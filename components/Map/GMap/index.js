17945f788f56e0623ed88a3051f5aa8dc917edd9
Daniel Leczycki <the242@gmail.com>
2023-07-25 09:50:05 -0400

    api key public

--- components/Map/GMap/index.js
+++ components/Map/GMap/index.js
@@ -8,7 +8,7 @@ import './style.scss'
 
 function GMap(props) {
 
-  const gMapKey = process.env.GOOGLE_API_KEY
+  const gMapKey = process.env.NEXT_PUBLIC_GOOGLE_API_KEY
 
   const defaultProps = {
     center: {
