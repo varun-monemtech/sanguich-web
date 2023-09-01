31e76e5cf3fdf8ed589604e52ad39bdf8a2c5259
leczyckibartek <leczyckibartek@gmail.com>
2023-09-01 10:14:41 +0200

    Style: Locations placed in tiles

--- components/ACF/Sanguich/Address/index.js
+++ components/ACF/Sanguich/Address/index.js
@@ -18,7 +18,7 @@ function Address(props) {
 
     return (
 
-      <div key={i} className="span-6 span-12-tablet grid-12">
+      <div key={i} className="span-6 span-12-tablet grid-12 tile">
 
         <div className="span-12 relative aspect-ratio" >
           <Image
@@ -34,15 +34,17 @@ function Address(props) {
         <div className="content-container span-12">
           <h6 className='uppercase font3 heading'>{node.name}</h6>
           <p className={`uppercase ${node.map ? 'underline' : ''}  m-top-off`}>
-            {node?.map?.place_id ? <a target='_blank' href={`https://www.google.com/maps/place/?q=place_id:${node?.map?.place_id}`}>{node.address}</a> : node.address}</p>
+            {node?.map?.place_id ? <a target='_blank' href={`https://www.google.com/maps/place/?q=place_id:${node?.map?.place_id}`}>{node.address}</a> : <span className='padd-top block'>{node.address}</span>}
+          </p>
           <p>
+            {i === 1 ? "NO DINE-IN | " : null}
             {node.phone? <a className="lighten-green" href={`tel:${node.phone}`}>{`${ph1} ${ph2} ${ph3} |`}</a> : null}
             <a className="green uppercase" href={`mailto:${node.mail}`}>{` ${node.mail}`}</a>
           </p>
           <p className="uppercase marg-bottom-off">{node.hours}</p>
           {/* <p className="green-border">CLOSED ON WEDNESDAYS</p> */}
         </div>
-        <div className='span-12 flex padd-bottom-3'>
+        <div className='span-12 flex padd-bottom'>
           <MultiButton links={node.links} />
         </div>
       </div>
@@ -61,8 +63,11 @@ function Address(props) {
           {anchor ?
             <div id={anchor} className="anchor"></div>
             : null}
+            <div className='span-12 heading-section'>
+              <h2 className="title font2"><span className="capitalize">L</span>ocations</h2>
+            </div>
 
-          <div className={"double span-12 span-12-mobile grid-12"}>
+          <div className={"double main-grid span-12 span-12-mobile grid-12"}>
             {itemsMap}
           </div>
 
