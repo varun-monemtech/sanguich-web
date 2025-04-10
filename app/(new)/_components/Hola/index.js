04af164bdc23011dd53883ebf63580077ed41900
leczyckibartek <leczyckibartek@gmail.com>
2025-04-10 14:58:05 +0200

    Hola Video Section

---
+++ app/(new)/_components/Hola/index.js
@@ -0,0 +1,41 @@
+
+import VideoComponent from '@/components/new/Video'
+import './style.scss'
+
+function Hola(props) {
+
+	const anchor = props.anchor
+	const classes = props.classes
+	const image = props.img
+	const video = props.video_file?.url
+
+	return (
+		<>
+			{classes ?
+				<section id={`section-${anchor}`} className={`c4 is-inview ${classes}`}>
+
+					{anchor ?
+						<div id={anchor} className="anchor"></div>
+						: null}
+
+					<VideoComponent
+						url={video}
+						// imgCover={{url: image.url, alt: ''}}
+						mode="inline"
+						wrapperClasses="light"
+						overlay={
+							{
+								color: '#44444466',
+								content: "<span class='hola'></span>",
+								// classes:"",
+							}
+						}
+					/>
+
+				</section>
+				: null}
+		</>
+	)
+}
+
+export default Hola
