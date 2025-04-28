b543db71f2f361c4658b5901e4d27148b2741600
Daniel Leczycki <the242@gmail.com>
2025-04-28 19:55:19 +0200

    svg animation

--- app/(new)/_components/Hero.tsx
+++ app/(new)/_components/Hero.tsx
@@ -2,6 +2,7 @@
 import { useRef } from 'react'
 import { motion, useScroll, useTransform } from 'framer-motion'
 import { LoadImage } from '@/components/new/LoadImage'
+import HeroTitleAnimated from './HeroTitleAnimated'
 
 function HeroNew() {
 	const containerRef = useRef(null)
@@ -84,123 +85,118 @@ function HeroNew() {
 					<div className="decor-bottom-right"></div>
 				</div>
 			</div>
-<div className='frs-grid z-[1001] relative'>
-			<motion.div
-				className="max-w-[15em] md:max-w-[20em]  lg:max-w-4xl  mx-auto w-full rounded-sm z-[200] relative"
-				initial={{ opacity: 0, translateY: 100 }}
-				animate={{ opacity: 1, translateY: 0 }}
-				transition={{
-					duration: 1,
-					translateY: { type: "spring", visualDuration: 0.4, bounce: 0.25 },
-				}}
-				style={{
-					y: titleParallax
-				}}
-				ref={titleRef}
-			>
-				<LoadImage
-					src='/hero-title.svg'
-					alt='sandwich'
-					width={500}
-					height={500}
-					className='[&_img]:relative '
-				/>
-			</motion.div>
 
-			<motion.div
-				className=" px-3 md:px-8 lg:px-24 mx-auto w-full top-24 rounded-sm z-10 max-lg:pt-12"
-				style={{
-					y: bigSandwichParallax
-				}}
-				ref={bigSandwichRef}
-			>
-				<LoadImage
-					src='/images/sandwiches/big.png'
-					alt='sandwich'
-					width={500}
-					height={500}
-					className='[&_img]:relative '
-				/>
-			</motion.div>
-
-			<div className='grid md:py-10 grid-cols-2 md:grid-cols-3 max-lg:pb-20 gap-2 lg:gap-24 mx-4 lg:mx-32 relative mt-20 z-10'>
+			<div className='frs-grid z-[1001] relative'>
 				<motion.div
-					style={{
-						y: sandwich1Parallax
+					className="max-w-[15em] md:max-w-[20em]  lg:max-w-4xl  mx-auto w-full rounded-sm z-[200] relative"
+					initial={{ opacity: 0, translateY: 100 }}
+					animate={{ opacity: 1, translateY: 0 }}
+					transition={{
+						duration: 2,
+						translateY: { type: "spring", visualDuration: 0.4, bounce: 0.25 },
 					}}
-					ref={sandwich1Ref}
-					transition={{ type: 'spring', stiffness: 100, damping: 30 }}
-					className='mt-10'
+					style={{
+						y: titleParallax
+					}}
+					ref={titleRef}
 				>
-					<LoadImage
-						src='/images/sandwiches/1.png'
-						alt='sandwich'
-						width={500}
-						height={500}
-						className='[&_img]:relative drop-shadow-[0_13px_13px_rgba(0,0,0,0.57)]'
-					/>
+					<HeroTitleAnimated className="w-full h-auto" />
 				</motion.div>
 
 				<motion.div
+					className=" px-3 md:px-8 lg:px-24 mx-auto w-full top-24 rounded-sm z-10 max-lg:pt-12"
 					style={{
-						y: sandwich2Parallax
+						y: bigSandwichParallax
 					}}
-					ref={sandwich2Ref}
-					transition={{ type: 'spring', stiffness: 100, damping: 30 }}
-					className='mt-5'
+					ref={bigSandwichRef}
 				>
 					<LoadImage
-						src='/images/sandwiches/2.png'
+						src='/images/sandwiches/big.png'
 						alt='sandwich'
 						width={500}
 						height={500}
-						className='[&_img]:relative drop-shadow-[0_13px_13px_rgba(0,0,0,0.57)]'
+						className='[&_img]:relative '
 					/>
 				</motion.div>
 
-				<motion.div
-					ref={sandwich3Ref}
-					style={{
-						y: sandwich3Parallax
-					}}
-					transition={{ type: 'spring', stiffness: 100, damping: 30 }}
-				>
-					<LoadImage
-						src='/images/sandwiches/3.png'
-						alt='sandwich'
-						width={500}
-						height={500}
-						className='[&_img]:relative drop-shadow-[0_13px_13px_rgba(0,0,0,0.57)]'
-					/>
-				</motion.div>
-			</div>
+				<div className='grid md:py-10 grid-cols-2 md:grid-cols-3 max-lg:pb-20 gap-2 lg:gap-24 mx-4 lg:mx-32 relative mt-20 z-10'>
+					<motion.div
+						style={{
+							y: sandwich1Parallax
+						}}
+						ref={sandwich1Ref}
+						transition={{ type: 'spring', stiffness: 100, damping: 30 }}
+						className='mt-10'
+					>
+						<LoadImage
+							src='/images/sandwiches/1.png'
+							alt='sandwich'
+							width={500}
+							height={500}
+							className='[&_img]:relative drop-shadow-[0_13px_13px_rgba(0,0,0,0.57)]'
+						/>
+					</motion.div>
 
-			<motion.div
-				className="absolute py-10 bottom-[0%] left-0 w-[200%] z-[-10]"
-				style={{
-					y: maskParallax,
-					x: maskTranslateX
-				}}
-				transition={{ type: 'spring', stiffness: 50 }}
-				ref={maskRef}
-			>
-				<div className="flex">
-					<LoadImage
-						src='/mask.svg'
-						alt='sandwich'
-						width={500}
-						height={500}
-						className='[&_img]:relative drop-shadow-[0_13px_13px_rgba(0,0,0,0.57)] pr-10'
-					/>
-					<LoadImage
-						src='/mask.svg'
-						alt='sandwich'
-						width={500}
-						height={500}
-						className='[&_img]:relative drop-shadow-[0_13px_13px_rgba(0,0,0,0.57)]'
-					/>
+					<motion.div
+						style={{
+							y: sandwich2Parallax
+						}}
+						ref={sandwich2Ref}
+						transition={{ type: 'spring', stiffness: 100, damping: 30 }}
+						className='mt-5'
+					>
+						<LoadImage
+							src='/images/sandwiches/2.png'
+							alt='sandwich'
+							width={500}
+							height={500}
+							className='[&_img]:relative drop-shadow-[0_13px_13px_rgba(0,0,0,0.57)]'
+						/>
+					</motion.div>
+
+					<motion.div
+						ref={sandwich3Ref}
+						style={{
+							y: sandwich3Parallax
+						}}
+						transition={{ type: 'spring', stiffness: 100, damping: 30 }}
+					>
+						<LoadImage
+							src='/images/sandwiches/3.png'
+							alt='sandwich'
+							width={500}
+							height={500}
+							className='[&_img]:relative drop-shadow-[0_13px_13px_rgba(0,0,0,0.57)]'
+						/>
+					</motion.div>
 				</div>
-			</motion.div>
+
+				<motion.div
+					className="absolute py-10 bottom-[0%] left-0 w-[200%] z-[-10]"
+					style={{
+						y: maskParallax,
+						x: maskTranslateX
+					}}
+					transition={{ type: 'spring', stiffness: 50 }}
+					ref={maskRef}
+				>
+					<div className="flex">
+						<LoadImage
+							src='/mask.svg'
+							alt='sandwich'
+							width={500}
+							height={500}
+							className='[&_img]:relative drop-shadow-[0_13px_13px_rgba(0,0,0,0.57)] pr-10'
+						/>
+						<LoadImage
+							src='/mask.svg'
+							alt='sandwich'
+							width={500}
+							height={500}
+							className='[&_img]:relative drop-shadow-[0_13px_13px_rgba(0,0,0,0.57)]'
+						/>
+					</div>
+				</motion.div>
 			</div>
 		</section>
 	)
