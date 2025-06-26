ac539849644f602d05cbf4b42919dbbcf7910864
Daniel Leczycki <the242@gmail.com>
2025-06-26 21:03:45 +0200

    border quickfix

--- app/(new)/_components/OurStory/index.js
+++ app/(new)/_components/OurStory/index.js
@@ -15,7 +15,7 @@ function OurStory(props) {
 			{classes ?
 				<section id={`section-${anchor}`} className={`frs-grid-full relative content border-type-11 c5 ${classes}`}>
 					<Intro delay={50}>
-						<div className="decor-wrap filter-invert opacity-[0.35]">
+						<div className="decor-wrap filter-invert opacity-[1] mix-blend-luminosity">
 							<div className="decor-top with-extra">
 								<div className="decor-top-left before:hidden"></div>
 								<div className="decor-top-center with-extra">
--- app/(new)/layout.tsx
+++ app/(new)/layout.tsx
@@ -62,6 +62,7 @@ export default async function RootLayout({
 			<head>
 			<meta httpEquiv='x-ua-compatible' content='ie=edge' />
 			<meta name="theme-color" content="#151515" />
+			<meta name="apple-mobile-web-app-title" content="Sanguich" />
 
 			<style>{`
           html {
