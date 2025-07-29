4e5d3a7a17c7ac811682fc185b281afaec5d426d
leczyckibartek <leczyckibartek@gmail.com>
2025-07-29 10:29:22 +0200

    Refactor Hero component layout and animations for improved structure and performance, including enhanced visibility transitions for images and updated parallax effects.

--- app/(new)/_components/Hero.tsx
+++ app/(new)/_components/Hero.tsx
@@ -66,146 +66,166 @@ function HeroNew() {
 		>
 			<Link href="#homepage-menu" title="Scroll To Menu" aria-label='Scroll To Menu' className="to-next 
 				absolute bottom-[0] translate-x-[-50%] left-1/2  max-h-[3.5em] z-[1005] 
-				arrow-bottom-image  mb-[-1em]"	
+				arrow-bottom-image  mb-[-1em]"
 			/>
 			<Intro delay={50}>
-			<div className="decor-wrap !p-0">
-				<div className="decor-top with-extra">
-					<div className="decor-top-left before:hidden"></div>
-					<div className="decor-top-center with-extra">
-						<div className="decor-top-center-extra-left"></div>
-						<div className="decor-top-center-extra-right"></div>
+				<div className="decor-wrap !p-0">
+					<div className="decor-top with-extra">
+						<div className="decor-top-left before:hidden"></div>
+						<div className="decor-top-center with-extra">
+							<div className="decor-top-center-extra-left"></div>
+							<div className="decor-top-center-extra-right"></div>
+						</div>
+						<div className="decor-top-right"></div>
 					</div>
-					<div className="decor-top-right"></div>
-				</div>
-				<div className="decor-center">
-					<div className="decor-center-left"></div>
-					<div className="decor-center-right"></div>
-				</div>
-				<div className="decor-bottom">
-					<div className="decor-bottom-left"></div>
-					<div className="decor-bottom-center with-extra">
-						<div className="decor-bottom-center-extra-left"></div>
-						<div className="decor-bottom-center-extra-right"></div>
+					<div className="decor-center">
+						<div className="decor-center-left"></div>
+						<div className="decor-center-right"></div>
 					</div>
-					<div className="decor-bottom-right"></div>
+					<div className="decor-bottom">
+						<div className="decor-bottom-left"></div>
+						<div className="decor-bottom-center with-extra">
+							<div className="decor-bottom-center-extra-left"></div>
+							<div className="decor-bottom-center-extra-right"></div>
+						</div>
+						<div className="decor-bottom-right"></div>
+					</div>
 				</div>
-			</div>
 
-			<div className='frs-grid z-[1001] relative pt-32 '>
-				<motion.div
-					className="max-w-[15em] md:max-w-[20em]  lg:max-w-4xl  mx-auto w-full rounded-sm z-[200] relative"
-					initial={{ opacity: 0, translateY: 100 }}
-					animate={{ opacity: 1, translateY: 0 }}
-					transition={{
-						duration: 2,
-						translateY: { type: "spring", visualDuration: 0.4, bounce: 0.25 },
-					}}
-					style={{
-						y: titleParallax
-					}}
-					ref={titleRef}
-				>
-					<HeroTitleAnimated className="w-full h-auto" />
-				</motion.div>
-
-				<motion.div
-					className=" px-0 md:px-8 lg:px-0 mx-auto w-full top-24 z-10 max-lg:pt-12"
-					style={{
-						y: bigSandwichParallax
-					}}
-					ref={bigSandwichRef}
-				>
-					<LoadImage
-						src='/images/sandwiches/sandwich-big.jpg'
-						alt='sandwich'
-						width={500}
-						height={500}
-						className='[&_img]:relative rounded-[58px] md:rounded-[281px] aspect-[21/9] md:aspect-[21/7] border-2 border-[#DCBA7B]'
-					/>
-				</motion.div>
-
-				<div className='md:py-10 flex max-md:flex-wrap justify-center max-lg:pb-20  lg:gap-36 mx-0 lg:mx-10 relative  z-10'>
+				<div className='frs-grid z-[1001] relative pt-32 '>
 					<motion.div
-						style={{
-							y: sandwich1Parallax
+						className="max-w-[15em] md:max-w-[20em]  lg:max-w-4xl  mx-auto w-full rounded-sm z-[200] relative"
+						initial={{ opacity: 0, translateY: 100 }}
+						animate={{ opacity: 1, translateY: 0 }}
+						transition={{
+							duration: 2,
+							translateY: { type: "spring", visualDuration: 0.4, bounce: 0.25 },
 						}}
-						ref={sandwich1Ref}
-						transition={{ type: 'spring', stiffness: 100, damping: 30 }}
-						className='md:mt-10 basis-1/3  md:basis-1/3 max-lg:px-[0.25rem] drop-shadow-[0_13px_13px_rgba(0,0,0,0.57)]'
+						style={{
+							y: titleParallax
+						}}
+						ref={titleRef}
 					>
-						<LoadImage
-							src='/images/sandwiches/1.png'
-							alt='sandwich'
-							width={500}
-							height={500}
-							className='[&_img]:relative overflow-visible '
-						/>
+						<HeroTitleAnimated className="w-full h-auto" />
 					</motion.div>
 
 					<motion.div
+						className=" px-0 md:px-8 lg:px-0 mx-auto w-full top-24 z-10 max-lg:pt-12"
+						initial={{ visibility: "hidden" }}
+						animate={{ visibility: "visible" }}
 						style={{
-							y: sandwich2Parallax
+							y: bigSandwichParallax
 						}}
-						ref={sandwich2Ref}
-						transition={{ type: 'spring', stiffness: 100, damping: 30 }}
-						className='max-md:-mt-5 md:mt-5 basis-1/3   md:basis-1/3 max-lg:px-[0.25rem] drop-shadow-[0_13px_13px_rgba(0,0,0,0.57)]'
+						ref={bigSandwichRef}
 					>
-						<LoadImage
-							src='/images/sandwiches/2.png'
-							alt='sandwich'
-							width={500}
-							height={500}
-							className='[&_img]:relative  overflow-visible '
-						/>
+						<Intro delay={50}
+						className='
+						[&.not-in-view_.content]:opacity-0
+						[&.not-in-view_.content]:translate-y-[4rem]
+						[&.in-view_.content]:opacity-100
+						[&.in-view_.content]:translate-y-[0%]
+						[&.in-view_.content]:ease-in-out
+						[&.in-view_.content]:duration-500
+						'
+						>
+
+							<LoadImage
+								src='/images/sandwiches/sandwich-big.jpg'
+								alt='sandwich'
+								width={500}
+								height={500}
+								className='[&_img]:relative rounded-[58px] md:rounded-[281px] aspect-[21/9] md:aspect-[21/7] border-2 border-[#DCBA7B]'
+							/>
+						</Intro>
+
 					</motion.div>
+					<div className='md:py-10 flex max-md:flex-wrap justify-center max-lg:pb-20  lg:gap-36 mx-0 lg:mx-10 relative  z-10'>
+						<motion.div
+							initial={{ visibility: "hidden" }}
+							animate={{ visibility: "visible" }}
+							style={{
+								y: sandwich1Parallax
+							}}
+							ref={sandwich1Ref}
+							transition={{ type: 'spring', stiffness: 100, damping: 30 }}
+							className='md:mt-10 basis-1/3  md:basis-1/3 max-lg:px-[0.25rem] drop-shadow-[0_13px_13px_rgba(0,0,0,0.57)]'
+						>
+							<LoadImage
+								src='/images/sandwiches/1.png'
+								alt='sandwich'
+								width={500}
+								height={500}
+								className='[&_img]:relative overflow-visible '
+							/>
+						</motion.div>
+
+						<motion.div
+							initial={{ visibility: "hidden" }}
+							animate={{ visibility: "visible" }}
+							style={{
+								y: sandwich2Parallax
+							}}
+							ref={sandwich2Ref}
+							transition={{ type: 'spring', stiffness: 100, damping: 30 }}
+							className='max-md:-mt-5 md:mt-5 basis-1/3   md:basis-1/3 max-lg:px-[0.25rem] drop-shadow-[0_13px_13px_rgba(0,0,0,0.57)]'
+						>
+							<LoadImage
+								src='/images/sandwiches/2.png'
+								alt='sandwich'
+								width={500}
+								height={500}
+								className='[&_img]:relative  overflow-visible '
+							/>
+						</motion.div>
+
+						<motion.div
+							initial={{ visibility: "hidden" }}
+							animate={{ visibility: "visible" }}
+							ref={sandwich3Ref}
+							style={{
+								y: sandwich3Parallax
+							}}
+							transition={{ type: 'spring', stiffness: 100, damping: 30 }}
+							className='basis-1/3 md:basis-1/3 max-lg:px-[0.25rem] drop-shadow-[0_13px_13px_rgba(0,0,0,0.57)]'
+						>
+							<LoadImage
+								src='/images/sandwiches/3.png'
+								alt='sandwich'
+								width={500}
+								height={500}
+								className='[&_img]:relative  overflow-visible '
+							/>
+						</motion.div>
+					</div>
 
 					<motion.div
-						ref={sandwich3Ref}
+						className="absolute py-10 bottom-[12%] md:bottom-[0%] left-0 w-[340%] md:w-[200%] z-[-10]"
 						style={{
-							y: sandwich3Parallax
+							y: maskParallax,
+							x: maskTranslateX
 						}}
-						transition={{ type: 'spring', stiffness: 100, damping: 30 }}
-						className='basis-1/3 md:basis-1/3 max-lg:px-[0.25rem] drop-shadow-[0_13px_13px_rgba(0,0,0,0.57)]'
+						transition={{ type: 'spring', stiffness: 50 }}
+						ref={maskRef}
 					>
-						<LoadImage
-							src='/images/sandwiches/3.png'
-							alt='sandwich'
-							width={500}
-							height={500}
-							className='[&_img]:relative  overflow-visible '
-						/>
+						<div className="flex">
+							<LoadImage
+								src='/mask.svg'
+								alt='sandwich'
+								width={500}
+								height={500}
+								className='h-full w-full [&_img]:h-full [&_img]:w-full [&_img]:relative drop-shadow-[0_13px_13px_rgba(0,0,0,0.57)] pr-10'
+							/>
+							<LoadImage
+								src='/mask.svg'
+								alt='sandwich'
+								width={500}
+								height={500}
+								className='h-full w-full  [&_img]:h-full [&_img]:w-full [&_img]:relative drop-shadow-[0_13px_13px_rgba(0,0,0,0.57)]'
+							/>
+						</div>
 					</motion.div>
 				</div>
-
-				<motion.div
-					className="absolute py-10 bottom-[12%] md:bottom-[0%] left-0 w-[340%] md:w-[200%] z-[-10]"
-					style={{
-						y: maskParallax,
-						x: maskTranslateX
-					}}
-					transition={{ type: 'spring', stiffness: 50 }}
-					ref={maskRef}
-				>
-					<div className="flex">
-						<LoadImage
-							src='/mask.svg'
-							alt='sandwich'
-							width={500}
-							height={500}
-							className='h-full w-full [&_img]:h-full [&_img]:w-full [&_img]:relative drop-shadow-[0_13px_13px_rgba(0,0,0,0.57)] pr-10'
-						/>
-						<LoadImage
-							src='/mask.svg'
-							alt='sandwich'
-							width={500}
-							height={500}
-							className='h-full w-full  [&_img]:h-full [&_img]:w-full [&_img]:relative drop-shadow-[0_13px_13px_rgba(0,0,0,0.57)]'
-						/>
-					</div>
-				</motion.div>
-			</div>
-		</Intro>
+			</Intro>
 
 
 		</section>
