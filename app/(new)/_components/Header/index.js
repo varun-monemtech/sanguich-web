5ad694a27d1c1342fd598d0c867e25f37023f600
Daniel Leczycki <the242@gmail.com>
2025-08-07 16:44:43 +0200

    order color

--- app/(new)/_components/Header/index.js
+++ app/(new)/_components/Header/index.js
@@ -154,8 +154,8 @@ function Header(props) {
 				unmountOnExit
 				nodeRef={nodeRef4}
 			>
-				<div ref={nodeRef4} className="fixed z-[1001] w-full flex justify-center max-md:top-[60px] max-lg:top-[80px] drop-shadow-[0_-2rem_0_#2b4b2e]">
-					<Link href="/#address" className="w-full py-2 px-6 bg-[#2b4b2e] uppercase rounded-b-3xl font-intensa text-[#D0C8B9] text-center font-bold drop-shadow-[0_3px_6px_rgba(0,0,0,0.16)]">
+				<div ref={nodeRef4} className="fixed z-[1001] w-full flex justify-center max-md:top-[60px] max-lg:top-[80px] drop-shadow-[0_-2rem_0_#274f38]">
+					<Link href="/#address" className="w-full py-2 px-6 bg-[#274f38] uppercase rounded-b-3xl font-intensa text-[#D0C8B9] text-center font-bold drop-shadow-[0_3px_6px_rgba(0,0,0,0.16)]">
 						<h5>
 							Order Now
 						</h5>
